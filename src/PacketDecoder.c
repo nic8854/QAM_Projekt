f617741ab35f3f798ffc3aef06042c16a67c7ca9
nic8854 <40262536+nic8854@users.noreply.github.com>
2025-12-09 20:28:10 +0100

    PacketDecoder.c: create packet queue

--- src/PacketDecoder.c
+++ src/PacketDecoder.c
@@ -1,7 +1,21 @@
 #include "PacketDecoder.h"
 
 #include "math.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 
-void InitPacketDecoder(){
+static QueueHandle_t packetQueue = NULL;
 
+
+void PacketDecoder_init() {
+    packetQueue = xQueueCreate(100, sizeof(uint64_t));
+}
+
+bool PacketDecoder_receivePacket(uint64_t packet) {
+    if (packetQueue == NULL) {
+        return false;
+    }
+    
+    bool result = xQueueSend(packetQueue, &packet, 0);
+    return result;
 }
