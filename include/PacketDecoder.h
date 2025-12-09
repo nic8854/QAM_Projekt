bf12176769535b3d0370e0416200aa917d532d78
nic8854 <40262536+nic8854@users.noreply.github.com>
2025-12-09 19:59:57 +0100

    PacketDecoder: add PacketDecoder_receivePacket prototype

--- include/PacketDecoder.h
+++ include/PacketDecoder.h
@@ -1,3 +1,6 @@
 #pragma once
+#include <stdbool.h>
 
 void PacketDecoder_init();
+
+bool PacketDecoder_receivePacket(uint64_t packet);
--- src/PacketDecoder.c
+++ src/PacketDecoder.c
@@ -2,6 +2,10 @@
 
 #include "math.h"
 
-void PacketDecoder_init(){
+void PacketDecoder_init() {
+
+}
+
+PacketDecoder_receivePacket(uint64_t packet) {
 
 }
