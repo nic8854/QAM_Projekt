ae920adeb62d3ba955eae10bd06c606a50ea804d
Sandro Alder, T-CH <sandro.alder@tecan.com>
2025-12-19 16:16:52 +0100

    Frequenz verdoppelt
    Von 78.125 auf 156.25Hz
    Sollte besser sein für RF Audiomodul
    Signal wird sonst zu sehr verzogen

--- components/eduboard2/eduboard2_config.h
+++ components/eduboard2/eduboard2_config.h
@@ -37,9 +37,9 @@
 // #define CONFIG_ADC_DEBUG
 #ifdef CONFIG_ENABLE_ADC_STREAMING
     #define CONFIG_ADC_STREAMING_BUFFERSIZE 256
-    // #define ADC_STREAM_SAMPLERATE_US    50
+     #define ADC_STREAM_SAMPLERATE_US    50
     // #define ADC_STREAM_SAMPLERATE_US    60
-     #define ADC_STREAM_SAMPLERATE_US    100
+    // #define ADC_STREAM_SAMPLERATE_US    100
     // #define ADC_STREAM_SAMPLERATE_US    120
     // #define ADC_STREAM_SAMPLERATE_US    200
     // #define ADC_STREAM_SAMPLERATE_US    240
@@ -54,9 +54,9 @@
     #define CONFIG_DAC_STREAMING
     #ifdef CONFIG_DAC_STREAMING
         #define CONFIG_DAC_STREAMING_BUFFERSIZE 128
-        // #define DAC_STREAM_SAMPLERATE    50
+         #define DAC_STREAM_SAMPLERATE    50
         // #define DAC_STREAM_SAMPLERATE    60
-         #define DAC_STREAM_SAMPLERATE    100
+        // #define DAC_STREAM_SAMPLERATE    100
         // #define DAC_STREAM_SAMPLERATE    120
         // #define DAC_STREAM_SAMPLERATE    200
         // #define DAC_STREAM_SAMPLERATE    240
