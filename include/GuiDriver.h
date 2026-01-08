f0d47d6e0c345c4e87db2194d401acb408f990cc
Remba92 <remo.bachmann@juventus.schule>
2026-01-08 20:48:57 +0100

    if def added

--- include/GuiDriver.h
+++ include/GuiDriver.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <stdbool.h>
+#include "eduboard2.h"
 
+#if defined(QAM_TX_MODE) || (defined(QAM_TRX_MODE) && defined(TRX_ROUTE_PACKET))
 
 void GuiDriver_init(void);
 bool GuiDriver_receiveTemperature(float temperature);
 bool GuiDriver_receiveText(char text[4]);
+
+#endif
